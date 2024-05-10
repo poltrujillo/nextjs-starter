1e39e3fd998bce90c8aa0b349fd707bb8e4aca7f
Pol Trujillo <103571473+poltrujillo@users.noreply.github.com>
2024-05-10 12:05:43 +0200

    Add shadcn/ui library component for ui

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,13 @@
 import { useTranslations } from 'next-intl';
+import { Button } from '@/components/ui/button';
 
 export default function Index() {
   const t = useTranslations('Index');
-  return <h1>{t('title')}</h1>;
+
+  return (
+    <div className="p-5">
+      <h1>{t('title')}</h1>
+      <Button>Button</Button>
+    </div>
+  );
 }
