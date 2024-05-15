b511468583e2ec55557f92c33c5c5e4795333f07
Pol Trujillo <103571473+poltrujillo@users.noreply.github.com>
2024-05-15 22:45:06 +0200

    Delete db schema

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,16 @@
 import { useTranslations } from 'next-intl';
 import { Button } from '@/components/ui/button';
 import { ModeToggle } from '@/components/mode-toggle';
-import { toast } from 'sonner';
 
 export default function Index() {
   const t = useTranslations('Index');
 
   return (
     <div>
-      <Button>{t('btn')}</Button>
+      <Button className="font-poppins">{t('btn')}</Button>
+      <Button variant="secondary" className="font-poppins">
+        {t('btnSecondary')}
+      </Button>
       <ModeToggle />
     </div>
   );
